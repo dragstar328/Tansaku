0b4ad912b1abc56e08bdd1e46328ecf99b6f6655
anonymous <a@b.com>
2018-08-09 18:09:25 +0900

    Add count add rebuild

--- word/urls.py
+++ word/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
   path('', views.portal, name="word_portal"),
+  path('rebuild', views.rebuild_model, name="rebuild_model"),
 ]
--- word/views.py
+++ word/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import WordForm
 
@@ -10,25 +10,35 @@ from collections import defaultdict
 # Create your views here.
 file_path = "../../Data/tansaku_meishi.model"
 #file_path = "../../Data/tansaku.model"
-word_model = word2vec.Word2Vec.load(file_path)
-keyed_model = KeyedVectors.load(file_path)
+#word_model = word2vec.Word2Vec.load(file_path)
+#keyed_model = KeyedVectors.load(file_path)
 
+def load_model():
+    global word_model
+    word_model = word2vec.Word2Vec.load(file_path)
+    global keyed_model
+    keyed_model = KeyedVectors.load(file_path)
+
+load_model()
 
 from sklearn.cluster import KMeans
 def make_cluster():
     vocab = list(keyed_model.wv.vocab.keys())[:3000]
     vectors = [keyed_model.wv[word] for word in vocab]
-    n_clusters = 20
+    n_clusters = 15
     kmeans_model = KMeans(n_clusters=n_clusters, verbose=1, random_state=42, n_jobs=-1)
     kmeans_model.fit(vectors)
 
     cluster_labels = kmeans_model.labels_
+
     cluster_to_words = defaultdict(list)
     for cluster_id, word in zip(cluster_labels, vocab):
         cluster_to_words[cluster_id].append(word)
-    return cluster_to_words
+    
+    global cluster
+    cluster = cluster_to_words
 
-cluster = make_cluster()
+make_cluster()
 
 def portal(request):
 
@@ -52,12 +62,26 @@ def portal(request):
     return render(request, 'word/portal.html', {'form': form, 'similars': similars, 'clusters': clusters})
 
 
+def rebuild_model(request):
+    load_model()
+    make_cluster()
+
+    return redirect('word_portal')
+
+
 def get_word_table(word):
     try:
+        ret = []
         res = word_model.wv.most_similar(positive=[word])
         print("similars")
         for result in res:
             print(result)
+            print(result[0])
+            print(word_model.wv.vocab[result[0]])
+            ret.append([result[0], round(result[1], 5), word_model.wv.vocab[result[0]].count])
+
+        print(ret)
+        return ret
     except KeyError:
         print(f"word {word} not in vocablary")
         #res = [(f"word [{word}] not in vocablafy")]
@@ -65,11 +89,17 @@ def get_word_table(word):
 
     return res
 
+def count_vocab(word):
+  return word_model.wv.vocab[word].count
+
 def get_cluster_table(word):
     for words in cluster.values():
         if word in words:
-           print(words)
-           return words
+            res = []
+            for w in words:
+                res.append([w, count_vocab(w)])
+            print(words)
+            return res
     return []
 
 
